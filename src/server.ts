7c3f7b03d148db4853ef0b2150e56fb8725d5316
Dindb-dong <dongwook443@yonsei.ac.kr>
2025-02-24 19:11:41 +0900

    feat: errorHandler

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoutes from './routes/authRoutes';
+import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandler';
 
 dotenv.config();
 const app = express();
 
+app.use(cors({ origin: 'https://poke-dungeon.vercel.app', credentials: true }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
@@ -13,6 +16,8 @@ mongoose.connect(process.env.MONGO_URI || '', { dbName: 'poke-dungeon' })
   .then(() => console.log('✅ MongoDB connected successfully'))
   .catch(err => console.error('❌ MongoDB connection failed:', err));
 
+// ✅ **에러 처리 미들웨어 (맨 아래에 추가해야 함)**
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server is running on port ${PORT}`);
